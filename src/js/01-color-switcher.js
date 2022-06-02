8daa14301349da03e752c8f8ea309df8d19ee8d7
Yurii Denkovych <95170165+Denkovych@users.noreply.github.com>
2022-06-02 19:44:16 +0300

    Add files via upload

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,19 @@
+const btnStart = document.querySelector('button[data-start]');
+console.log(btnStart);
+const btnStop = document.querySelector('button[data-stop]');
+console.log(btnStop);
+
+btnStart.addEventListener('click', ()=>{
+    intId =  setInterval(()=>{
+    document.body.style.backgroundColor = getRandomHexColor();
+    } , 1000)
+    btnStart.disabled = true;
+});
+btnStop.addEventListener('click', ()=>{
+    clearInterval(intId); 
+    btnStart.disabled = false;  
+});
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+const refs = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+  start: document.querySelector('[data-start]'),};
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+let selected ={};
+
+refs.start.disabled = true;
+const options = {
+  
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    
+const dateNow = Date.now();
+
+    if(selectedDates[0].getTime() <= dateNow){
+      //const selectedDate = selectedDates[0].getTime();
+      refs.start.disabled = true;
+      return window.alert("Please choose a date in the future")} 
+    else{
+        refs.start.disabled = false;
+        
+        }
+        
+       
+ refs.start.addEventListener('click', timer(selectedDates[0]));
+}}
+
+flatpickr('#datetime-picker', options);
+
+
+function timer(e){
+
+
+ 
+
+
+   setInterval(()=>{
+    const currentTime = Date.now();
+    const selectDate = e.getTime();
+    const result = selectDate  - currentTime;
+    const convertResult = convertMs(result);
+    
+    refs.days.textContent = convertResult.days;
+    refs.hours.textContent = convertResult.hours;
+    refs.minutes.textContent = convertResult.minutes;
+    refs.seconds.textContent = convertResult.seconds;  
+  }, 1000)
+}
+
+function pad(value){
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
