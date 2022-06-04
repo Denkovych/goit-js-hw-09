aa0efc44dba7447874f6dbe3734ccc718e5b5491
Yurii Denkovych <yd2004@ukr.net>
2022-06-04 11:59:33 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,10 +8,12 @@ btnStart.addEventListener('click', ()=>{
     document.body.style.backgroundColor = getRandomHexColor();
     } , 1000)
     btnStart.disabled = true;
+    btnStop.disabled =false;
 });
 btnStop.addEventListener('click', ()=>{
     clearInterval(intId); 
-    btnStart.disabled = false;  
+    btnStart.disabled = false; 
+    btnStop.disabled =true; 
 });
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
